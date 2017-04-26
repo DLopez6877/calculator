a6c3d29a7fbf40fe93300ccee8f4070e546d3719
Daniel Lopez and Michael Koceja <daniel@>
2017-04-26 13:30:24 -0700

    fixed spacing in index.html, simplify functions and add jquerry event to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,28 @@
-// var tempConvert = function(celsius1) {
-// 	return celsius1 * 1.8 + 32;
-// }
-//
-// var celsius1 = parseFloat(prompt("Enter Celsius Temperature"));
-// var results = tempConvert(celsius1);
-// // alert(results);
+// Business (or back-end) logic:
 
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = subtract(number1, number2);
-alert(result);
-
 var multiply = function(number1, number2) {
   return number1 * number2;
-}
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = multiply(number1, number2);
-alert(result);
+};
 
 var divide = function(number1, number2) {
   return number1 / number2;
-}
+};
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = divide(number1, number2);
-alert(result);
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
